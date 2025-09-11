9567227594eace2e79068f91507c4f2c4201cd6c
Aravind Rajendran <aravind.rajendrn@tama.systems>
2025-09-12 00:43:23 +0530

    feat(sprint2): add core DocTypes (Company, Project, Task, Project Member, Task Checklist)

--- pm_app/pm_suite/doctype/project/test_project.py
+++ pm_app/pm_suite/doctype/project/test_project.py
@@ -2,21 +2,8 @@
 # See license.txt
 
 # import frappe
-from frappe.tests import IntegrationTestCase
+from frappe.tests.utils import FrappeTestCase
 
 
-# On IntegrationTestCase, the doctype test records and all
-# link-field test record dependencies are recursively loaded
-# Use these module variables to add/remove to/from that list
-EXTRA_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
-IGNORE_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
-
-
-
-class IntegrationTestProject(IntegrationTestCase):
-	"""
-	Integration tests for Project.
-	Use this class for testing interactions between multiple components.
-	"""
-
+class TestProject(FrappeTestCase):
 	pass
--- pm_app/pm_suite/doctype/project_member/project_member.py
+++ pm_app/pm_suite/doctype/project_member/project_member.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class Project(Document):
+class ProjectMember(Document):
 	pass
