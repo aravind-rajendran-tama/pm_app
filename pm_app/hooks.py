1b5343b30d9bc452c17bd86103fa73b67ec5a655
Aravind Rajendran <aravind.rajendran@tama.systems>
2025-09-30 03:28:05 +0530

    fix(fixtures): add type field to Workspace fixtures

--- pm_app/hooks.py
+++ pm_app/hooks.py
@@ -244,7 +244,7 @@ fixtures = [
     # Only include NON-standard (custom) web templates
     {"doctype": "Web Template", "filters": [["standard", "=", 0]]},
     {"doctype": "Portal Settings"},
-    # {"doctype": "Workspace"},  # temporarily disabled
+    {"doctype": "Workspace"},
     {"doctype": "Dashboard Chart"},
     {"doctype": "Notification"},
     {"doctype": "Report"},
